800bfe8428b3954d9b29a0b66aad53341d1a1180
Guillermo <151547321+GuillermoTSA@users.noreply.github.com>
2024-04-09 07:53:00 -0300

    intercambiar.cs
    09/04/2024

---
+++ intercambiar.cs
@@ -0,0 +1,16 @@
+class Intercambiar
+{
+    static void Main(string[] args)
+    {
+        int valor1 = 40;
+        int valor2 = 12;
+        inter(ref valor1, ref valor2);
+        Console.WriteLine("Valor 1: " + valor1 + " Valor 2: " + valor2);
+    }
+    static void inter(ref int a, ref int b)
+    {
+        int c = a;
+        a = b;
+        b = c;
+    }
+}
