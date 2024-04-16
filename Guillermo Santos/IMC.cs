c06ce771684addd06deef691d5e76483a51fd8c4
Guillermo <151547321+GuillermoTSA@users.noreply.github.com>
2024-04-16 07:08:46 -0300

    Create IMC.cs

---
+++ Guillermo Santos/IMC.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese su altura");
+        float Alt = float.Parse(Console.ReadLine());
+        //Utilizamos Console.ReadLine para capturar la cadena de caracteres que el usuario ingrese en la consola, la parseamos y la guardamos en una variable
+        Console.WriteLine("Ingrese su peso");
+        float Wgh = float.Parse(Console.ReadLine());
+
+        float Cuenta = Wgh / (Alt * Alt);
+        //Realizamos la cuenta necesaria para conocer nuestro situacion con respecto a los límites del índice de masa corporal
+
+        if (Cuenta < 18.5)
+            Console.WriteLine("Usted está por debajo de el indice de masa corporal recomendado");
+        if (Cuenta > 25.0)
+            Console.WriteLine("Usted está por sobre el índice de masa corporal recomendado");
+        if (Cuenta > 18.5 && Cuenta < 25.0)
+            Console.WriteLine("Usted está dentro del índice de masa recomendado");
+        //Utilizamos la estructura de bifurcación if para condicionar nuestro resultado a los datos ingresados por el usuario
+    }
+
+}
