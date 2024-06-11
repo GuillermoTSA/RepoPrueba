857e051631eceaa2ce51f63cc23e456db878c35f
Guillermo <151547321+GuillermoTSA@users.noreply.github.com>
2024-06-11 08:28:43 -0300

    Update MainWindows.xaml.cs

--- Guillermo Santos/wpf/Champions/MainWindows.xaml.cs
+++ Guillermo Santos/wpf/Champions/MainWindows.xaml.cs
@@ -26,14 +26,27 @@ namespace WpfApp2
 
             List<Equipos> ListaEquipos = new List<Equipos>();
 
-            ListaEquipos.Add(new Equipos() { Equipo1 = "Real Madrid", Equipo2 = "Bayern", CampeonatosEuropa1 = "15", CampeonatosEuropa2 = "6" });
-            ListaEquipos.Add(new Equipos() { Equipo1 = "PSG", Equipo2 = "Manchester City", CampeonatosEuropa1 = "0", CampeonatosEuropa2 = "1" });
-            ListaEquipos.Add(new Equipos() { Equipo1 = "Arsenal", Equipo2 = "Juventus", CampeonatosEuropa1 = "0", CampeonatosEuropa2 = "2" });
-            ListaEquipos.Add(new Equipos() { Equipo1 = "ACMilan", Equipo2 = "Barcelona", CampeonatosEuropa1 = "7", CampeonatosEuropa2 = "5" });
+            ListaEquipos.Add(new Equipos() { Equipo1 = "Real Madrid", Equipo2 = "Bayern", CampeonatosEuropa1 = "16", CampeonatosEuropa2 = "6", Diferencia = "10" });
+            ListaEquipos.Add(new Equipos() { Equipo1 = "PSG", Equipo2 = "Manchester City", CampeonatosEuropa1 = "0", CampeonatosEuropa2 = "1", Diferencia = "1" });
+            ListaEquipos.Add(new Equipos() { Equipo1 = "Arsenal", Equipo2 = "Juventus", CampeonatosEuropa1 = "0", CampeonatosEuropa2 = "2", Diferencia = "2" });
+            ListaEquipos.Add(new Equipos() { Equipo1 = "ACMilan", Equipo2 = "Barcelona", CampeonatosEuropa1 = "7", CampeonatosEuropa2 = "5", Diferencia = "2" });
 
             ListBoxEquipos.ItemsSource = ListaEquipos;
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListBoxEquipos.SelectedItem != null)
+            {
+            MessageBox.Show(
+            (ListBoxEquipos.SelectedItem as Equipos).Equipo1 + " " + (ListBoxEquipos.SelectedItem as Equipos).CampeonatosEuropa1 + " " + (ListBoxEquipos.SelectedItem as Equipos).Equipo2 + " " + (ListBoxEquipos.SelectedItem as Equipos).CampeonatosEuropa2);
+            }
+            else
+            {
+                MessageBox.Show("Usted debe seleccionar una fila");
+            
+            }
+        }
     }
 
     public class Equipos
@@ -42,5 +55,6 @@ namespace WpfApp2
         public string Equipo2 { get; set; }
         public string CampeonatosEuropa1 { get; set; }
         public string CampeonatosEuropa2 { get; set; }
+        public string Diferencia { get; set; }
     }
 }
